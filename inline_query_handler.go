7cf6c353819e3143fcbf20b8bf3a3c42eaa37628
Dmitry Romanov <me@wutiarn.ru>
2017-01-22 18:55:24 +0300

    Write poll topic and variants to redis

--- inline_query_handler.go
+++ inline_query_handler.go
@@ -31,8 +31,14 @@ var resultArticlesContent = []ResultArticleContent{
 	},
 }
 
+var expirationTime = 60*60*24*10
+
 func HandleInlineQuery(ctx context.Context, query *telegram.InlineQuery) (err error) {
 	api := telebot.GetAPI(ctx)
+	redis, err := GetRedisConnection()
+	if err != nil {
+		return err
+	}
 
 	resultQueryArticles := []telegram.InlineQueryResult{}
 
@@ -43,6 +49,14 @@ func HandleInlineQuery(ctx context.Context, query *telegram.InlineQuery) (err er
 			return err
 		}
 
+		pollTopic := fmt.Sprintf("*%v*", query.Query)
+
+		topicKey := fmt.Sprintf("openpoll_%s_topic", pollId)
+		variantsKey := fmt.Sprintf("openpoll_%s_variant", pollId)
+
+		redis.Do("SET", topicKey, pollTopic)
+		redis.Do("EXPIRE", topicKey, expirationTime)
+
 		buttons := []telegram.InlineKeyboardButton{}
 
 		for i, replyVariant := range resultArticleContent.variants {
@@ -51,7 +65,10 @@ func HandleInlineQuery(ctx context.Context, query *telegram.InlineQuery) (err er
 				Text:         emoji.Sprint(replyVariant),
 			}
 			buttons = append(buttons, button)
+
+			redis.Do("RPUSH", variantsKey, replyVariant)
 		}
+		redis.Do("EXPIRE", variantsKey)
 
 		replyMarkup := telegram.InlineKeyboardMarkup{
 			InlineKeyboard: [][]telegram.InlineKeyboardButton{
@@ -64,7 +81,7 @@ func HandleInlineQuery(ctx context.Context, query *telegram.InlineQuery) (err er
 				Type: "article",
 				ID:   pollId,
 				InputMessageContent: telegram.InputTextMessageContent{
-					MessageText: fmt.Sprintf("*%v*", query.Query),
+					MessageText: pollTopic,
 					ParseMode:   "Markdown",
 				},
 				ReplyMarkup: replyMarkup,
@@ -72,7 +89,7 @@ func HandleInlineQuery(ctx context.Context, query *telegram.InlineQuery) (err er
 			Title: emoji.Sprintf(resultArticleContent.title),
 		}
 
-		// TODO: Register poll in redis
+		logrus.Info("Created poll %v for query %v", pollId, query.ID)
 
 		resultQueryArticles = append(resultQueryArticles, resultArticle)
 	}
